2fc30a2d989c72637daf67cf943cd1481534e68e
Adrian Torres <adriantorres.gc@gmail.com>
2025-04-07 10:37:15 +0100

    modified:   Reto_1 Agenda_contactos/funciones.py

--- Reto_1 Agenda_contactos/funciones.py
+++ Reto_1 Agenda_contactos/funciones.py
@@ -1,18 +1,49 @@
 class Contacto:
     """Informacion de un contacto.
-    nombre  --str el cual tendra dicho nombre de usuario
-    telefono    --int el cual tendra dicho numero de telefono
-    email       --str el cual tedra el correo del usuario"""
+    Clase que representa un contacto con nombre, teléfono y email.
+
+    nombre      --str -- nombre de usuario
+    telefono    --str -- numero de telefono a futuro sera un --int--
+    email       --str -- correo del usuario"""
 
     def __init__(self,nombre,telefono,email):
+        """inicializa un nuevo contacto.
+
+        Args:
+        nombre      --str -- nombre de usuario
+        telefono    --str -- numero de telefono
+        email       --str -- correo del usuario"""
+
         self.nombre = nombre
         self.telefono = telefono
         self.email = email
 
     def __str__(self):
+        """Devuelve los datos del contacto en una cadena.
+
+        Return:
+        --str--:Datos del contactos formateados."""
+
         return(F"Nombre: {self.nombre}, Telefono: {self.telefono}, Email: {self.email}")
     
 
-usuario= Contacto("Paka",555_444_333,"esta@aqui.es")
+#usuario= Contacto("Paka",555_444_333,"esta@aqui.es")   """borrar al finalizar"""
 
-print(usuario)
+#print(usuario)                                         """borrar al finalizar"""
+
+class GestorContactos:
+    """Clase que gestiona una lista de contactos y almacena en un archivo .txt.
+   
+    Atributos:
+    archivo     --str--: ruta del archivo txt.
+    contactos   --lista[Contacto]--: Lista de objetos Contacto.
+    """
+
+    def __init__(self,archivo="contactos.txt"):
+        """Inicia el gestor de carga de  los contactos en el archivo.txt
+        Args:
+        archivo --str--:nombre del archivo de almacenamiento (si quieres otro cambialo aqui)
+        """
+        self.archivo =archivo
+        self.contactos = []
+        self.cargar_contactos()
